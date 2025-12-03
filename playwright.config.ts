f433a879ee0891e9a25f8e5e326dbefff3a97df5
TestVagrant <kajalt@testvagrant.com>
2025-12-03 12:09:51 +0530

    added email password

---
+++ playwright.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from '@playwright/test';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig({
+  testDir: './tests',
+  use: {
+    baseURL: 'https://www.facebook.com', 
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
+});
